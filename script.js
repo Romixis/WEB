3be960483c8335bc8c7e0cf3b56888b48278a5d6
Romixis <roma_ilyukhin@mail.ru>
2022-12-05 13:48:15 +0700

    Lab7: готова.

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("9");
+
+const numberOne = 7;
+const numberTwo = 8;
+console.log(numberOne);
+console.log(numberTwo);
+
+console.log(numberOne + numberTwo);
+
+const word = "Задание";
+console.log(word.toUpperCase());
+console.log(word.toLowerCase());
+
+console.log(numberOne + numberTwo);
+console.log(numberOne);
+console.log(numberOne * numberTwo);
+console.log(numberOne / numberTwo);
+console.log(numberOne ** 2); 
+console.log(numberOne % numberTwo); 
+
+//  Выведет случайное число от 0 до 100.
+console.log("Случайное значение:", Math.round(Math.random() * 101))
+
+//  Шестой элемент из массива.
+let fruits = ["47", "85", "7", "2", "76", "11", "49", "28", "37", "95"];
+console.log(fruits[5]);
